49b5384aa62e01ee582c53b313e0280f72f48478
4KFILL <phillipskrasnov@yandex.ru>
2025-05-21 17:22:31 +0300

    branch_1 - first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Получаем ссылку на кнопку
+        const button = document.getElementById('btn');
+        
+        // Добавляем обработчик события click
+        button.addEventListener('click', function() {
+            alert('Привет! Это alert окно.');
+        });
